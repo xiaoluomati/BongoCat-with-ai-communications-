6459224d7663278a3d571df1bcd212a1169346c0
xiaoluomati <1109988978@qq.com>
2026-05-21 12:10:42 +0800

    fix: import nextTick from vue in chat store

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import { invoke } from '@tauri-apps/api/core'
 import { listen, type UnlistenFn } from '@tauri-apps/api/event'
 
